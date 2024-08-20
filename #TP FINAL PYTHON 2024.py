3e1d5418d5edbfa0d7f6e879ad54ed83862667d8
nutrialau <avilau@hotmail.com>
2024-08-20 15:02:09 -0300

    Subi los primeros dos ejercicios y copie algunas consignas

---
+++ #TP FINAL PYTHON 2024.py
@@ -0,0 +1,33 @@
+#TP FINAL PYTHON 2024
+
+""" 
+#Ejercicio 1: Números y cadenas de carácteres
+ ## 1) Enteros
+print('Hola soy programa que te va a pedir dos números enteros')
+numero_1=int(input('Escribí un número entero: '))
+numero_2=int(input('Escribí otro número entero: '))
+print('La suma de los dos números elegidos es: '+str(numero_1+numero_2))
+print('El producto de los dos números elegidos es: '+str(numero_1*numero_2))
+print('La concatenacion de los dos números elegidos es: '+str(numero_1)+str(numero_2)) """
+ ## 2) Cadena
+print('Hola soy un programa que te va a pedir una cadena de texto')
+cadena_de_texto=input('Ingresa una frase u oración que contenga la letra A: ')
+print('Tu cadena elegida en mayúsculas: '+cadena_de_texto.upper())
+print('Longitud de la cadena elegida: '+str(len(cadena_de_texto)))#LONGITUD
+print('La cadena al revés se lee: '+cadena_de_texto[::-1])#CADENA INVERTIDA
+print('La cantidad de veces que aparece la letra a: '+ str(cadena_de_texto.count('a')))#CANTIDAD DE VECES QUE APARECE 'A'
+ ## 3) Decimal a binario
+ ## 4) Cadena y número entero
+#Ejercicio 2: Listas y Tuplas
+## 1) Lista de tres frutas (añadir dos frutas mas, ordenar la lista alfabeticamente, mostrar la lista entera, eliminar una fruta y mostrar el resultado)
+## 2) Tuple con los nombres de dos ciudades (mostrar el primer y el ultimo elemento, convertir la tupla en una lista (añadir una nueva ciudad) y mostrar la lista resultante)
+## 3) Crear una lista con numeros enteros y mostrar : el mayor y el menor numero de la lista, y el promedio de los numeros de la lista
+## 4) programa que reciba una lista de cadenas y muestre la lista con todas las cadenas en mayusculas
+#Ejercicio 3: Controladores de flujo
+## 1) Escribir un programa que pida un numero al usuario y muestre si el numero es par o impar
+## 2) Programa que simule un menú simple con las opciones i_Saludar, ii_Despedirse, iii_Salir; y dependiendo de la opcion elegida muestre un mensaje distinto, y que si se elige la opcion iii el programa debe finalizar.
+## 3) Pide un numero al usuario y que determine si el numero es positivo, negativo o cero.
+## 4) Mediante FOR, un programa que muestre los numeros del 1 al 10.
+## 5) Mediante WHILE, programa que calcule la suma de los numeros del 1 al 100.
+#Ejercicio 5: Conjuntos y diccionarios
+## 1)
